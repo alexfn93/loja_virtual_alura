0913b7879efeb20361fa37ef4b09c1a857730c29
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2023-08-12 10:35:14 -0300

    first commit

---
+++ src/br/com/alura/jdbc/TestaConexao.java
@@ -0,0 +1,20 @@
+package br.com.alura.jdbc;
+
+import br.com.alura.jdbc.factory.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+
+		System.out.println("Fechando conexo!!");
+
+		connection.close();
+	}
+
+}
