1743b533e3286ef7f4bbad9ed29a60d24017916d
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2023-08-12 21:46:26 -0300

    refatorei a classe 'ConnectionFactory' inserindo o pool de conexões

--- src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,26 @@
 package br.com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("25@!<>llIllI");
+
+		this.dataSource = comboPooledDataSource;
+	}
+
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager
-				.getConnection
-				("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-						"root", "25@!<>llIllI");
+		return this.dataSource.getConnection();
 	}
 }
