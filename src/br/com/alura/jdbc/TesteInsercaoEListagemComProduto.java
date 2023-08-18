84af84a34e0b2b80f834c15cc4dc074fd9056d36
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2023-08-17 23:12:45 -0300

    refatorei classe DAO e TesteInsercaoEListagemComProduto

--- src/br/com/alura/jdbc/TesteInsercaoEListagemComProduto.java
+++ src/br/com/alura/jdbc/TesteInsercaoEListagemComProduto.java
@@ -1,25 +1,24 @@
 package br.com.alura.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
 import br.com.alura.jdbc.modelo.Produto;
 import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.factory.ConnectionFactory;
 
-public class TesteInsercaoComProduto {
+public class TesteInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 
-		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
+		Produto comoda = new Produto("Desktop", "Desktop para Notebooks");
 
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			ProdutoDAO produtoDao = new ProdutoDAO(connection);
 			produtoDao.salvar(comoda);
-			// Lista = persistenciaProduto.listar();
+			List<Produto> listaDeProdutos = produtoDao.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
 	}
 }
