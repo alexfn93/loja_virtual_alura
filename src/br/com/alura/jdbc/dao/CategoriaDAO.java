a4b147667f0f0662d82fe289516378170b8ca9ca
Alex Fernandes <108309097+alexfn93@users.noreply.github.com>
2023-08-20 23:09:53 -0300

    corrigi a classe CategoriaDAO

--- src/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -40,24 +40,22 @@ public class CategoriaDAO {
 	public List<Categoria> listarComProdutos() throws SQLException {
 		Categoria ultima = null;
 		List<Categoria> categorias = new ArrayList<>();
-
-		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN"
-				+ " PRODUTO P ON C.ID = P.CATEGORIA_ID";
-
+		
 		System.out.println("Executando a query de listar categoria");
+
+		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN PRODUTO P ON C.ID = P.CATEGORIA_ID";
 
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 			pstm.execute();
 
 			try (ResultSet rst = pstm.getResultSet()) {
 				while (rst.next()) {
-					if (ultima == null || ultima.getNome().equals(rst.getString(2))) {
+					if (ultima == null || !ultima.getNome().equals(rst.getString(2))) {
 						Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
 						ultima = categoria;
 						categorias.add(categoria);
 					}
-					Produto produto
-					= new Produto (rst.getInt(3), rst.getString(4), rst.getString(5));
+					Produto produto = new Produto(rst.getInt(3), rst.getString(4), rst.getString(5));
 					ultima.adicionar(produto);
 				}
 			}
